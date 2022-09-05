0db6f0331820f7fc165ed94094a9aa9ad6654c67
Jakob Frank <e11837319@student.tuwien.ac.at>
2022-09-05 17:26:55 +0200

    Implemented updateValidation and setStatus

--- budget-backend/src/main/java/frankie/financebudget/service/inputValidation/EntryInputValidator.java
+++ budget-backend/src/main/java/frankie/financebudget/service/inputValidation/EntryInputValidator.java
@@ -1,14 +1,12 @@
 package frankie.financebudget.service.inputValidation;
 
 import frankie.financebudget.entities.entities.objects.Entry;
-import frankie.financebudget.entities.enumerations.EntryType;
+import frankie.financebudget.persistence.EntryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,12 +18,19 @@ public class EntryInputValidator {
     private double maxAmount = Double.MAX_VALUE;
     private double minAmount = -Double.MAX_VALUE;
     private LocalDate today = LocalDate.now();
-    private int status = 200;
-    private StringBuilder message = new StringBuilder("Message: \n");
+    private int status;
+    private StringBuilder message;
 
-    public EntryInputValidator() {}
+    private final EntryDAO entryDAO;
+
+    @Autowired
+    public EntryInputValidator(EntryDAO entryDAO) {
+        this.entryDAO = entryDAO;
+    }
 
     public boolean createValidation(Entry toValidate) {
+        this.status = 200;
+        this.message = new StringBuilder("Message: \n");
 
         //check id
         if (toValidate.getId() != null) {
@@ -93,6 +98,75 @@ public class EntryInputValidator {
     }
 
     public boolean updateValidation(Entry toValidate) {
+        this.status = 200;
+        this.message = new StringBuilder("Message: \n");
+
+        //check id
+        if (toValidate.getId() == null) {
+            setStatus(400);
+            message.append("Id must be defined!\n");
+        }
+        if (toValidate.getId() <= 0) {
+            setStatus(409);
+            message.append("Id must be greater than 0");
+        }
+
+        //check amount
+        //check null
+        if (toValidate.getAmount() == null) {
+            setStatus(400);
+            message.append("Amount must not be null!\n");
+        }
+        //check if amount is a number
+        else if (toValidate.getAmount().isNaN()) {
+            setStatus(409);
+            message.append("Amount must be a defined number!");
+        }
+        else if (toValidate.getAmount() < minAmount || toValidate.getAmount() > maxAmount) {
+            setStatus(409);
+            message.append("Amount must be within " + minAmount + " - " + maxAmount + "!\n");
+        }
+
+        //check description
+        //check null
+        Pattern p = Pattern.compile("[ ]*");
+        Matcher m = p.matcher(toValidate.getDescription());
+
+        if (toValidate.getDescription() == null) {
+            setStatus(400);
+            message.append("Description must be given!\n");
+        }
+        //check if only whitespace
+        else if (m.matches()) {
+            setStatus(409);
+            message.append("Description must not only contain Whitespaces!\n");
+        }
+        //check maxlength
+        else if (toValidate.getDescription().length() > descriptionLength) {
+            setStatus(409);
+            message.append("Description is too Long! Max length is " + descriptionLength + " characters!\n");
+        }
+
+        //check dateCreated
+        if (toValidate.getDateCreated() == null) {
+            setStatus(400);
+            message.append("DateCreated must be defined!\n");
+        }
+        else if (toValidate.getDateCreated().isAfter(LocalDate.now())) {
+            setStatus(409);
+            message.append("DateCreated must not be in future!");
+        }
+
+        //check EntryType
+        if (toValidate.getType() == null) {
+            setStatus(400);
+            message.append("Type must be defined!\n");
+        }
+
+        if (this.status == 200) {
+            return true;
+        }
+
         return false;
     }
 
@@ -106,6 +180,9 @@ public class EntryInputValidator {
 
 
     private void setStatus(int status) {
+        if (this.status == 400 || this.status == 409) {
+            return;
+        }
         this.status = status;
     }
 
